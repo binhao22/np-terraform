c664f88260f1fe0bdc3a28785b1b73260809e360
binhao22 <binhao@naver.com>
2024-04-09 14:28:24 +0900

    lb

--- elb/main.tf
+++ elb/main.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "this" {
   for_each           = toset(var.subnets)
   load_balancer_type = "application"
   internal           = false
-  subnets            = each.value
+  subnets            = flatten([each.value])
   security_groups    = [ var.security_group ]
 
   tags = {
